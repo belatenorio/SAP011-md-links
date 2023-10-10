4e9270dfc9d6e05c8e34a36652316e18a06d02b0
belatenorio <117682146+belatenorio@users.noreply.github.com>
2023-10-10 16:03:28 -0300

    configs basicas package-json

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs');
+const chalk = require('chalk');
+
+// function lerArquivo(caminhoDoArquivo){
+//    return new Promise(function(resolve, reject){
+//     fs.readFile(caminhoDoArquivo, 'utf-8', (err, data) =>{
+//         if(err) reject err;
+//         resolve(data);
+//     });
+//     }) 
+// }
+
+console.log(chalk.red('Hello world!'));
+//console.log(chalk.blue(nome da constante));
+//console.log(chalk.bgRed("A soma é:"), chalk.blue(resultado));
+
+//construir uma função mdLinks que vai ser uma promise.
